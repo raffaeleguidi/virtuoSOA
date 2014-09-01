0f2669f6d9402a90cf15eb94a8b2468aad789576
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2014-09-01 19:01:02 +0200

    Added an attempt to a cachingfilter and an interceptor

--- app/utils/EmptyFilter.scala
+++ app/utils/EmptyFilter.scala
@@ -6,13 +6,13 @@ import scala.concurrent.Future
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object EmptyFilter extends Filter {
-
+  
 	def apply(nextFilter: (RequestHeader) => Future[Result])
-				(requestHeader: RequestHeader): Future[Result] = {	  
+			(requestHeader: RequestHeader): Future[Result] = {	  
 		nextFilter(requestHeader).map { result =>
-		  
-		  Logger.info(" ******************************** " + requestHeader.method)
+		  requestHeader.host 
+		  Logger.info(" ******************************* empty filter ******************************* " + requestHeader.method)
 		  result
 	    }
-	  }
+	}
 }
--- app/utils/Global.scala
+++ app/utils/Global.scala
@@ -1,8 +1,26 @@
 package utils
 
 import play.api.mvc._
+import models.Route
+import play.cache.Cache
 
 
 object Global extends WithFilters(LoggingFilter) {
-
+//    def findRoute(source: String) = {
+//    	Cache.getOrElse[Route]("route:" + source) {
+//		  	Route.find.where().eq("source", source).findUnique()
+//		}
+//    }
+//	val route = findRoute(rh.host) 
+//	val cachePrefix =  rh.method + ":" + Cache.getOrElse[Double]("source:" + route.randomSeed) {
+//		route.randomSeed
+//	}
+//	val cacheKey = cachePrefix + rh.host + rh.path + rh.rawQueryString 
+//		Cache.getOrElse[play.libs.F.Promise[play.api.mvc.Result]](cacheKey) {
+//			controllers.Proxy.asyncGetNoCache("ss")
+//		}
+	override def onRouteRequest(request: RequestHeader): Option[Handler] = {
+		println("executed before every request:" + request.toString)
+		super.onRouteRequest(request)
+	}
 }
